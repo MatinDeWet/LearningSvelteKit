9b597ddcee16307267a8022a99ab8dd78e588868
Matin de Wet <matindewet116@gmail.com>
2024-09-01 09:57:45 +0200

    navigation

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,5 +1,11 @@
 <script>
+	import { goto } from "$app/navigation";
+
     let name = 'about';
 </script>
 
 <h1>About {name}</h1>
+
+<button on:click={() => {
+    goto('/');
+}}>Go to Home</button>
