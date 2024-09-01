645f7c48a5ee664a8f18166336c2ba13f027e38b
Matin de Wet <matindewet116@gmail.com>
2024-09-01 14:40:58 +0200

    layout groups

---
+++ src/routes/(admin)/+layout.svelte
@@ -0,0 +1,4 @@
+<div style="background-color: blueviolet; padding: 10px;">
+	Admin
+	<slot />
+</div>
---
+++ src/routes/(frontend)/+layout.svelte
@@ -0,0 +1,10 @@
+<div style="background-color: cadetblue; padding: 10px;">
+	Frontend
+	<nav>
+		<a href="/">Home</a>
+		<a href="/about">About</a>
+		<a href="/settings">Settings</a>
+	</nav>
+
+	<slot />
+</div>
