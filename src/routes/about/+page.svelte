3fbcd1c34c11705552ac18fc0d6c2783ce788abe
Matin de Wet <matindewet116@gmail.com>
2024-09-01 09:36:13 +0200

    bacic routing

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,5 @@
+<script>
+    let name = 'about';
+</script>
+
+<h1>About {name}</h1>
---
+++ src/routes/product/[id]/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+	import { page } from '$app/stores';
+</script>
+
+<h1>Product Page</h1>
+<p>Product id: {$page.params.id}</p>
+
+<a href="/product/123">Other product</a>
