8f7099c25fd81921d3b8ea41d276e6ebbed075d4
Matin de Wet <matindewet116@gmail.com>
2024-09-01 14:29:39 +0200

    basic layouts

---
+++ src/routes/settings/+layout.svelte
@@ -0,0 +1,9 @@
+<div style="background-color: red; padding: 10px;">
+	<h3>Settings Layout</h3>
+	<nav>
+		<a href="/settings/general">General</a>
+		<a href="/settings/profile">Profile</a>
+	</nav>
+
+	<slot />
+</div>
